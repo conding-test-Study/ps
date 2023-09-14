815b9b10b5673fed2cf581ff1edd5c9a03de9530
1wooo <dnjsdn2468@naver.com>
2023-09-14 16:29:10 +0900

    자료구조-7-김원우

--- 자료구조/자료구조-7-김원우.py
+++ 자료구조/자료구조-7-김원우.py
@@ -10,6 +10,7 @@ def solution(bridge_length, weight, truck_weights):
     while tr:
         sec += 1
         while q and sec - q[0][1] == bridge_length:
+            #초당 1 이동하니까 현재 초를 기준으로 건너간 거리 계산 후 pop
             tmp = q.popleft()
             q_sum -= tmp[0]
 
@@ -18,14 +19,15 @@ def solution(bridge_length, weight, truck_weights):
             if q_sum + cur_tr <= weight:
                 q_sum += cur_tr
                 q.append([tr.popleft(), sec])
+                # 초를 사용해 지나간 거리 계산해야하므로 덱에 들어갈때 sec도 같이 삽입
 
         else:
             q_sum += cur_tr
             q.append([tr.popleft(), sec])
 
     while q:
+        # 덱에 남아있는 거 연산
         sec += 1
-
         if sec - q[0][1] == bridge_length:
             tmp = q.popleft()
 
