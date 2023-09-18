ed3ed4c2a1d36a9c274cd940221422bb83587cdf
jyzayu <moosou30@gmail.com>
2023-09-18 20:25:50 +0900

    Create 그리디-1-최종윤.py

---
+++ 그리디/그리디-1-최종윤.py
@@ -0,0 +1,20 @@
+#전에 풀었던 문제라서 갑자기 뒤 값만 작은거를 계속 고르면 되는게 떠오름 
+
+import sys
+input =  sys.stdin.readline
+n = int(input())
+
+c = []
+for i in range(n):
+    c.append(list(map(int,input().split())))
+
+c.sort(key = lambda x: (x[1], x[0]))
+
+ans = 0
+e = 0
+
+for i in range(n):
+    if e <= c[i][0]:
+        e = c[i][1]
+        ans += 1
+print(ans)
