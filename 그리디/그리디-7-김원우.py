83f5c5841cf2a046597c7b07795308b26375ebd3
1wooo <dnjsdn2468@naver.com>
2023-09-22 14:37:40 +0900

    그리디-7-김원우

--- 그리디/그리디-7-김원우.py
+++ 그리디/그리디-7-김원우.py
@@ -18,9 +18,17 @@ for start, destination, boxes in send:
     maxbox = boxes
 
     for i in range(start, destination):
+        # 주어진 박스 수 vs 해당 마을에서 실을 수 있는 최대 박스 수
+        # 둘 중 최솟값을 적용
         maxbox = min(maxbox, space - box[i])
 
     for i in range(start, destination):
+        # 적용한 최솟값은 다시 box배열에 반여해서 해당 마을에서 실을 수 있는 최대 박스 수 갱신
         box[i] += maxbox
+
     answer += maxbox
 print(answer)
+
+# box배열을 통해 출발 마을부터 도착지 마을까지 얼마나 싣고 가야할지 체크하는게 관건
+# 그리디인데 왠지 DP냄새가 살짝나는듯한 굉장히 어려운 문제같다..
+# 실전 코테에서 만났으면 절대 못풀었을듯;
