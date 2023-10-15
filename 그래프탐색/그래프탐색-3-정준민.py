700d5ede4b4102b6150b2762507fb39a6e1a3666
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-10-15 18:37:51 +0900

    그래프탐색-3-정준민 수정완료

--- 그래프탐색/그래프탐색-3-정준민.py
+++ 그래프탐색/그래프탐색-3-정준민.py
@@ -14,7 +14,6 @@ def printGraph(graph):
     print("@@@@@@@@@@@")
 
 def bfs(i, j):
-    global today
     res = deque()
     q = deque()
     res.append((i, j))
@@ -23,29 +22,25 @@ def bfs(i, j):
     cnt = 1
     sum = graph[i][j]
 
-    visit = [[False for _ in range(N)] for _ in range(N)]
     visit[i][j] = True
-    today[i][j] = True
     while q:
         x, y = q.popleft()
 
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            if nx<0 or ny<0 or nx>=N or ny>=N or visit[nx][ny] or today[nx][ny]: continue
+            if nx<0 or ny<0 or nx>=N or ny>=N or visit[nx][ny]: continue
 
             gap = abs(graph[x][y]-graph[nx][ny])
             if L <= gap <= R:
                 q.append((nx, ny))
                 res.append((nx, ny))
                 visit[nx][ny] = True
-                today[nx][ny] = True
                 cnt += 1
                 sum += graph[nx][ny]
 
     if cnt != 1:
         for x, y in res:
             graph[x][y] = sum//cnt  # 인구수 조정
-
     return cnt
 
 N, L, R = map(int, input().split())
@@ -55,44 +50,18 @@ for _ in range(N):
 
 
 ans = 0
-
+tmp = 1
 while True:
-    # 예외처리
-    # 하루에 동시에 이동 가능한 그룹이 2개 이상일 경우
-
-    # 4 10 50
-    # 10 100 20 90
-    # 80 100 60 70
-    # 70 20 30 40
-    # 50 20 100 10 에서
-
-    # 10 100 50 50
-    # 50 50 50 50
-    # 50 50 50 50
-    # 50 50 100 50 로 바뀌고 다음 날
-
-    # (30) 100 50 50
-    # (30) 50 50 50
-    # 50 50 (50) 50
-    # 50 (50) (100) (50) 괄호친 부분이 동시에 다 바뀌어야 하는데,
-    # 윗 그룹만 바뀌고 아래 그룹은 안바뀜..
-
-    # -> today 배열로 오늘 바뀐애들은 더이상 못건들게 처리
-
-    today = [[False for _ in range(N)] for _ in range(N)]
     flag = 0
+    visit = [[False for _ in range(N)] for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            tmp = bfs(i, j)
+            if not visit[i][j]: tmp = bfs(i, j)
             if tmp != 1:    # 한번이라도 이동이 있으면
-                # printGraph(graph)
                 flag = 1
 
     # 종료 조건 : 한번도 인구 조정이 일어나지 않았을 경우
-    printGraph(graph)
     if flag == 0:
         break
     else: ans += 1
-
-printGraph(graph)
 print(ans)
