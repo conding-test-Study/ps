5557e4386d5e6a83374ebdf7aad7638f71c9e073
jyzayu <moosou30@gmail.com>
2023-10-17 20:58:43 +0900

    Update 완전탐색-3-최종윤.py

--- 완전탐색/완전탐색-3-최종윤.py
+++ 완전탐색/완전탐색-3-최종윤.py
@@ -1,20 +1,10 @@
 #답지확인
-#세다가 5되면 바로 break하려니까 그렇지 왼쪽 오른쪾 다 포함해서 세면 cnt 6이 될텐데 왜 틀린거지?
+# 왼쪽 오른쪾 다 포함해서 세면 cnt 6이 될텐데 왜 틀린거지?
 
-#왼쪽위 같이 탐색하면 6목 잡을수있는거 아니야? 같이 세서 6목이면
-# 안 하면 되는 거잖아  대신 visited는  고려해야겠지 
 
 import sys
 input = sys.stdin.readline
 
-# 오른쪽아래 왼쪽위로 연속으로있는 개수 세기
-#오른쪽위 왼쪽아래로 연속 개수 세기
-#가로 연속 세기
-#세로 연속 세기
-#이렇게 4가지로 처음 셌었는데 오른쪽 아래로 내려가는 방향만 하면 되네 왼쪽 위에서 부터 하니까  
-#앞에 있는 건 다시 6목 체크하니까
-
-        
 d = [[1,1], [-1,1], [0,1], [1,0]]
 
 
@@ -38,7 +28,7 @@ for r,c in loc:
             if cnt ==5:
                 if 0 <= r - dr < 19 and 0 <= c - dc < 19 and arr[r][c] == arr[r - dr][c - dc]:
                     break
-                if 0 <= r + dr < 19 and 0 <= c + dc < 19 and arr[r][c] == arr[nr + dr][nc + dc]:
+                if 0 <= nr + dr < 19 and 0 <= nc + dc < 19 and arr[r][c] == arr[nr + dr][nc + dc]:
                     break
                 print(arr[r][c])
                 print(r+1, c+1)
@@ -46,5 +36,75 @@ for r,c in loc:
             nr += dr
             nc += dc
 print(0)
+
+
+#틀린 코드
+# 전에 풀었던 문제는 조합을 combi나 비트연산으로 만들었지만 이건 경우가 많으니 dfs가 좋지 않을까?
+
+
+
+import sys
+input = sys.stdin.readline
+
+# 오른쪽아래 왼쪽위로 연속으로있는 개수 세기
+
+#오른쪽위 왼쪽아래로 연속 개수 세기
+
+
+#가로 연속 세기
+
+#세로 연속 세기
+
+
+def dfs(r, c, d):
+    global cnt
+    visited[r][c] = True
+    cnt += 1
+
+    for dr, dc in d:
+        nr = r + dr
+        nc = c + dc
+        if 0 <= nr < 19 and 0 <= nc < 19:
+            if not visited[nr][nc] and arr[r][c] == arr[nr][nc]:
+                visited[nr][nc] = True
+                dfs(nr, nc, d)
         
 
+
+
+
+#10x10을 탐색 세번 하는 거니까 100 ^3이 아니라 x3같은데 나올수있는 경우는^3이지만
+
+d = [
+    [[1,1], [-1,-1]],
+    [[-1,1], [1,-1]],
+    [[0,1], [0,-1]],
+    [[1,0], [-1,0]],
+]
+
+arr = []
+loc = []
+for i in range(19):
+    data = (list(map(int,input().split())))
+    for j in range(19):
+        if data[j] == 1 or data[j] == 2:
+            loc.append([i, j])
+    arr.append(data)
+
+chk = False
+for r,c in loc:
+    for mode in d:
+        visited = [[False]*19 for i in range(19)]
+        cnt = 0
+        dfs(r, c, mode)
+        if cnt == 5:
+            print(arr[r][c])
+            print(r+1, c+1)
+            chk = True
+            break
+    if chk:
+        break
+if not chk:
+    print(0)
+        
+        
