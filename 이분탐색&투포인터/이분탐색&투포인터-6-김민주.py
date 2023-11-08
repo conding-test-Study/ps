0b57931f9b9a2b1d44b3cb2bed28df541a535600
Minjoo Kim <sjctk1@gmail.com>
2023-11-08 14:02:46 +0900

    이분탐색&투포인터 5, 6 풀이

---
+++ 이분탐색&투포인터/이분탐색&투포인터-6-김민주.py
@@ -0,0 +1,14 @@
+# 출처: https://kjhoon0330.tistory.com/entry/BOJ-22781-징검다리-건너기-Python [Jahni's Blog:티스토리]
+
+INF = 999999999
+n = int(input())
+A = list(map(int, input().split()))
+# dp[i]는 i까지 가는데 드는 최소 힘
+dp = [0] + [INF] * (n - 1)
+
+for i in range(1, n):
+    for j in range(0, i):
+        power = max((i - j) * (1 + abs(A[i] - A[j])), dp[j])
+        dp[i] = min(dp[i], power)
+
+print(dp[-1])
