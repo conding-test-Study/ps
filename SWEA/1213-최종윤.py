72eeb54d1fb4b4dee2eeb70c65ae6adf2f97d4e7
jyzayu <moosou30@gmail.com>
2023-11-09 17:11:03 +0900

    Update 1213-최종윤.py

--- SWEA/1213-최종윤.py
+++ SWEA/1213-최종윤.py
@@ -1,3 +1,7 @@
+#s.count(target)하면 그냥 풀리더라 문자만 세는 것이아닌 문자열을 세준다.
+#첫글자가 같은게 나오면 거기서부터 비교하기도 하더라
+#len(split(target))하면 target 수 +1 만큼 이니까 -1해서 구하기도 하더라
+
 T = 10
 for tc in range(1, T + 1):
     t = input()
